f76fea0aeb8dce6e247228d9b00c8bb9913d7ebc
adityaa369 <adityaamruthaluri369@gmail.com>
2026-02-11 01:10:15 +0530

    Add Procfile and .env.example for render deployment, update main.pyfor production

--- main.py
+++ main.py
@@ -423,4 +423,5 @@ if __name__ == '__main__':
     print("="*70 + "\n")
     
     # Run the application
-    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
+    app.run(debug=False)
+    
